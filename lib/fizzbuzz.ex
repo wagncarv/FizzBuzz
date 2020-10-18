3e857dcb16b607da366fa3dd32ebb2f4008fcc5d
wagner carvalho <wagncarv@gmail.com>
2020-10-17 21:57:05 -0300

    funcao build e handle_file_read criadas

---
+++ lib/fizzbuzz.ex
@@ -0,0 +1,11 @@
+defmodule Fizzbuzz do
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+  #
+  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}), do: result
+
+end
