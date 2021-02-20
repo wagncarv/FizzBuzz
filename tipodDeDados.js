c2f78b84a1de0a50584264d5b3f5d8ce90728927
Wagner Carvalho <wagncarv@gmail.com>
2021-02-20 15:17:51 -0300

    js

---
+++ tipodDeDados.js
@@ -0,0 +1,7 @@
+function exec(value){
+    const exec = document.querySelector("#exec")
+    exec.innerHTML = value
+}
+
+////////////////////////////////////////////////////////////////
+exec("olá")
